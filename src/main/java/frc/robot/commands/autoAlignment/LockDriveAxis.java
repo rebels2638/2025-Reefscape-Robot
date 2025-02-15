ffc66898de28aad6c16646c18c69d403889bf6c1
Edan Liahovetsky <edan.liahovetsky@icloud.com>
2025-02-15 17:54:38 +0100

    major alignment util cleanup, axis based closest source pose getter, and alliance flipping for all candidates

--- src/main/java/frc/robot/commands/autoAlignment/LockDriveAxis.java
+++ src/main/java/frc/robot/commands/autoAlignment/LockDriveAxis.java
@@ -2,12 +2,11 @@ package frc.robot.commands.autoAlignment;
 
 import java.util.Optional;
 import java.util.function.DoubleSupplier;
-import java.util.function.Supplier;
-
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -15,127 +14,15 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.RobotState;
 import frc.robot.lib.input.XboxController;
-import frc.robot.lib.util.RebelUtil;
+import frc.robot.lib.util.AlignmentUtil.Axis;
 import frc.robot.subsystems.drivetrain.swerve.SwerveDrive;
 import frc.robot.constants.Constants;
-import frc.robot.constants.Constants.AlignmentConstants;
 import frc.robot.constants.swerve.drivetrainConfigs.SwerveDrivetrainConfigBase;
 import frc.robot.constants.swerve.drivetrainConfigs.SwerveDrivetrainConfigComp;
 import frc.robot.constants.swerve.drivetrainConfigs.SwerveDrivetrainConfigProto;
 import frc.robot.constants.swerve.drivetrainConfigs.SwerveDrivetrainConfigSim;
 
 public class LockDriveAxis extends LinearDriveToPose {
-    public static void main(String[] args) {
-        Axis a = AlignmentConstants.kBARGE_AXIS;
-
-        Translation2d b = a.getPointOnAxis(new Translation2d(5,6));
-        System.out.println(b.getX() + " " + b.getY()); 
-
-    }
-    public static class Axis {
-        public final Double x1, y1, x2, y2;
-        public final Double m, b;
-        public Axis(Double x1, Double y1, Double x2, Double y2) {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-
-            if (x2.equals(x1)) {
-                this.m = Double.POSITIVE_INFINITY;
-                this.b = x1;
-            }
-            else {
-                this.m = (y2 - y1) / (x2 - x1);
-                this.b = y1 - m * x1;
-            }
-        }
-
-        public Axis(Double m, Double b) {
-            this.x1 = Double.NaN;
-            this.y1 = Double.NaN;
-            this.x2 = Double.NaN;
-            this.y2 = Double.NaN;
-
-            this.m = m;
-            this.b = b;
-        }
-
-        // will constrain between x1 and x2
-        public Double getOutput(double input) {
-            if (!Double.isNaN(x1) && !Double.isNaN(x2)) {
-                input = RebelUtil.constrain(input, x1, x2);
-            }
-            if (m.isInfinite()) {
-                return Double.POSITIVE_INFINITY;
-            }
-            return m * input + b;
-        }
-
-        public Axis getPerpendicularAxis(Double x3, Double y3) {
-            Double m2;
-            Double b2;
-            if (this.m.isInfinite()) {
-                m2 = 0.0;
-                b2  = y3;
-            }
-            else {
-                m2 = -1/this.m;
-                b2 = y3 - m2 * x3;
-            }
-
-            return new Axis(
-                m2,
-                b2
-            );
-        }
-
-        public Translation2d getIntersection(Axis other) {
-            if (this.m.equals(other.m) || this.m.isInfinite() && other.m.isInfinite()) {
-                return null;
-            }
-
-            Double x;
-            Double y;
-            if (this.m.isInfinite()) {
-               x = this.b;
-               y = other.getOutput(x);
-            }
-            else if (other.m.isInfinite()) {
-                x = other.b;
-                y = this.getOutput(x);
-            }
-            else {
-                x = (this.b - other.b) / (other.m - this.m);
-                y = getOutput(x);    
-            }
-
-            return new Translation2d(x, y);
-        }
-
-        public Translation2d getPointOnAxis(Translation2d p) {
-            Double x3 = p.getX();
-            Double y3 = p.getY();
-            
-            Axis perpendicularAxis = getPerpendicularAxis(x3, y3);
-            Translation2d intersection = getIntersection(perpendicularAxis);
-            if (intersection == null) {
-                intersection = p;
-            }
-
-            if (!Double.isNaN(x1) && !Double.isNaN(x2) &&
-                !Double.isNaN(y1) && !Double.isNaN(y2)) {
-                return new Translation2d(
-                    RebelUtil.constrain(intersection.getX(), x1, x2),
-                    RebelUtil.constrain(intersection.getY(), y1, y2)
-                );
-            }
-            else {
-                return intersection;
-            }
-        }
-    }
-
     private final SwerveDrive swerveDrive = SwerveDrive.getInstance();
     private final RobotState robotState = RobotState.getInstance();
     private final SwerveDrivetrainConfigBase drivetrainConfig;
@@ -143,14 +30,17 @@ public class LockDriveAxis extends LinearDriveToPose {
     private final DoubleSupplier vX, vY, heading; // Supplier functions for velocity inputs and heading.
     private int invert = 1;                       // Variable to invert direction based on alliance color.
     
-    private double previousTimestamp = Timer.getTimestamp();
+    private final Axis desiredAxis;
+    private final Rotation2d desiredRotation;
 
-    private Supplier<Pose2d> targetPose;
-    private Supplier<ChassisSpeeds> endVelo;
+    private double previousTimestamp = Timer.getTimestamp();
     
-    public LockDriveAxis(XboxController xboxDriver) {
+    public LockDriveAxis(XboxController xboxDriver, Axis desiredAxis, Rotation2d desiredRotation) {
         super(() -> RobotState.getInstance().getEstimatedPose(), () -> new ChassisSpeeds());
         
+        this.desiredAxis = desiredAxis;
+        this.desiredRotation = desiredRotation;
+
         switch (Constants.currentMode) {
             case COMP:
                 drivetrainConfig = SwerveDrivetrainConfigComp.getInstance();
@@ -208,27 +98,27 @@ public class LockDriveAxis extends LinearDriveToPose {
         double dt = Timer.getTimestamp() - previousTimestamp;
 
         ChassisSpeeds driverSpeeds = new ChassisSpeeds(
-            vX.getAsDouble() * drivetrainConfig.getMaxDrivetrainTranslationalVelocityMetersPerSec() * invert,
-            vY.getAsDouble() * drivetrainConfig.getMaxDrivetrainTranslationalVelocityMetersPerSec() * invert,
-            heading.getAsDouble() * drivetrainConfig.getMaxDrivetrainAngularVelocityRadiansPerSec()
+            vX.getAsDouble() * drivetrainConfig.getMaxTranslationalVelocityMetersPerSec() * invert,
+            vY.getAsDouble() * drivetrainConfig.getMaxTranslationalVelocityMetersPerSec() * invert,
+            heading.getAsDouble() * drivetrainConfig.getMaxAngularVelocityRadiansPerSec()
         );
 
         Pose2d closestPose = new Pose2d(
-            AlignmentConstants.kBARGE_AXIS.getPointOnAxis(
+            desiredAxis.getPointOnAxis(
                 robotState.getEstimatedPose().getTranslation()
             ),
-            AlignmentConstants.kBARGE_ROTATION
+            desiredRotation
         );
         Logger.recordOutput("LockDriveAxis/closestPose", closestPose);
 
         Pose2d nextPoint = new Pose2d(
-            AlignmentConstants.kBARGE_AXIS.getPointOnAxis(
+            desiredAxis.getPointOnAxis(
                 new Translation2d(
                     robotState.getEstimatedPose().getTranslation().getX() + driverSpeeds.vxMetersPerSecond * dt,
                     robotState.getEstimatedPose().getTranslation().getY() + driverSpeeds.vyMetersPerSecond * dt
                 )
             ),
-            AlignmentConstants.kBARGE_ROTATION
+            desiredRotation
         );
         Logger.recordOutput("LockDriveAxis/nextPoint", nextPoint);
 
--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -14,131 +14,205 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
-import frc.robot.commands.autoAlignment.LockDriveAxis.Axis;
+import edu.wpi.first.wpilibj.DriverStation;
+import frc.robot.lib.util.AlignmentUtil.Axis;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static final Mode currentMode = Mode.SIM; // TODO: change this if sim
-  // public static final boolean isSYSID = true; // TODO: change this if sysid
-  public static enum Mode {
-    /** Running on a real robot. */
-    COMP,
+    public static final Mode currentMode = Mode.SIM; // TODO: change this if sim
+    // public static final boolean isSYSID = true; // TODO: change this if sysid
 
-    PROTO,
+    public static enum Mode {
+        /** Running on a real robot. */
+        COMP,
 
-    /** Running a physics simulator. */
-    SIM,
+        PROTO,
 
-    /** Replaying from a log file. */
-    REPLAY
-  }
+        /** Running a physics simulator. */
+        SIM,
 
-  public static final double kLOOP_CYCLE_MS;
-  static {
-    switch (currentMode) {
-        case COMP:
-            kLOOP_CYCLE_MS = 0.02;
-
-            break;
-    
-        case PROTO:
-            kLOOP_CYCLE_MS = 0.02;
-
-            break;
-
-        case SIM:
-            kLOOP_CYCLE_MS = 0.02;
-
-            break;
-
-        case REPLAY:
-            kLOOP_CYCLE_MS = 0.02;
-
-            break;
-
-        default:
-            kLOOP_CYCLE_MS = 0.02;
-
-            break;
+        /** Replaying from a log file. */
+        REPLAY
     }
-  }
 
-  public static final class OperatorConstants {
-    public static final int kDRIVER_CONTROLLER_PORT = 0;
-
-    // Joystick Deadband
-    // yes, this high
-    public static final double LEFT_X_DEADBAND = 0.09;
-    public static final double LEFT_Y_DEADBAND = 0.09;
-    
-    public static final double RIGHT_X_DEADBAND = 0.09;
-    
-    private OperatorConstants() {}
-  }
-
-  public static final class AlignmentConstants {
-    // these assume the robot's volume is zero. does not take into account frame
-    public static final double kMAX_ALIGNMENT_DIST_METERS = 1.5;
-
-    public static final double kINTER_BRANCH_DIST_METER = 0.34;
-    
-    public static final Pose2d[] kCENTER_FACES =
-        new Pose2d[6]; // Starting facing the driver station in clockwise order
+    public static final double kLOOP_CYCLE_MS;
     static {
-      // Initialize faces
-      kCENTER_FACES[0] =
-        //bottom
-          new Pose2d(
-            3.642, 
-            4.024,
-            Rotation2d.fromDegrees(0));
-        //top right
-      kCENTER_FACES[1] =
-          new Pose2d(
-            4.916, 
-            3.285,
-            Rotation2d.fromDegrees(120));
-        //bottom right
-      kCENTER_FACES[2] =
-          new Pose2d(
-            4.064,
-            3.291,
-            Rotation2d.fromDegrees(60));
-        //top
-      kCENTER_FACES[3] =
-          new Pose2d(
-            5.344, 
-            4.023,
-            Rotation2d.fromDegrees(180));
-        //bottom left
-      kCENTER_FACES[4] =
-          new Pose2d(
-            4.064, 
-            4.763,
-            Rotation2d.fromDegrees(-60));
-        //top left
-      kCENTER_FACES[5] =
-          new Pose2d(
-            4.912, 
-            4.770,
-            Rotation2d.fromDegrees(-120));
+        switch (currentMode) {
+            case COMP:
+                kLOOP_CYCLE_MS = 0.02;
+
+                break;
+
+            case PROTO:
+                kLOOP_CYCLE_MS = 0.02;
+
+                break;
+
+            case SIM:
+                kLOOP_CYCLE_MS = 0.02;
+
+                break;
+
+            case REPLAY:
+                kLOOP_CYCLE_MS = 0.02;
+
+                break;
+
+            default:
+                kLOOP_CYCLE_MS = 0.02;
+
+                break;
+        }
     }
 
-    public static final Axis kBARGE_AXIS = new Axis(
-        8.232,
-        4.343,
-        8.232,
-        7.973
-    ); 
-    public static final Rotation2d kBARGE_ROTATION = new Rotation2d(0);
-    
-    private AlignmentConstants() {}
-  }
+    public static final class OperatorConstants {
+        public static final int kDRIVER_CONTROLLER_PORT = 0;
+
+        // Joystick Deadband
+        // yes, this high
+        public static final double LEFT_X_DEADBAND = 0.09;
+        public static final double LEFT_Y_DEADBAND = 0.09;
+
+        public static final double RIGHT_X_DEADBAND = 0.09;
+
+        private OperatorConstants() {
+        }
+    }
+
+    public static final class AlignmentConstants {
+        // these assume the robot's volume is zero. does not take into account frame
+        public static final double kMAX_ALIGNMENT_DIST_METERS = 1.5;
+
+        public static final double kINTER_BRANCH_DIST_METER = 0.34;
+
+        public static final Pose2d[] kREEF_CENTER_FACES = new Pose2d[6]; // Starting facing the driver station in clockwise
+                                                                    // order
+        static {
+            // Initialize faces
+            kREEF_CENTER_FACES[0] =
+                    // bottom
+                    new Pose2d(
+                            3.642,
+                            4.024,
+                            Rotation2d.fromDegrees(0));
+            // top right
+            kREEF_CENTER_FACES[1] = new Pose2d(
+                    4.916,
+                    3.285,
+                    Rotation2d.fromDegrees(120));
+            // bottom right
+            kREEF_CENTER_FACES[2] = new Pose2d(
+                    4.064,
+                    3.291,
+                    Rotation2d.fromDegrees(60));
+            // top
+            kREEF_CENTER_FACES[3] = new Pose2d(
+                    5.344,
+                    4.023,
+                    Rotation2d.fromDegrees(180));
+            // bottom left
+            kREEF_CENTER_FACES[4] = new Pose2d(
+                    4.064,
+                    4.763,
+                    Rotation2d.fromDegrees(-60));
+            // top left
+            kREEF_CENTER_FACES[5] = new Pose2d(
+                    4.912,
+                    4.770,
+                    Rotation2d.fromDegrees(-120));
+        }
+
+        public static final Pose2d[] kSOURCE_CENTER_FACES = new Pose2d[6]; 
+        static {
+            // right top
+            kSOURCE_CENTER_FACES[0] = new Pose2d(
+                1.281,
+                0.269,
+                Rotation2d.fromDegrees(54)
+            );
+
+            // right center
+            kSOURCE_CENTER_FACES[1] = new Pose2d(
+                0.845,
+                0.607,
+                Rotation2d.fromDegrees(54)
+            );
+
+            // right bottom
+            kSOURCE_CENTER_FACES[2] = new Pose2d(
+                0.423,
+                0.911,
+                Rotation2d.fromDegrees(54)
+            );
+
+            // left top
+            kSOURCE_CENTER_FACES[3] = new Pose2d(
+                1.281,
+                7.758,
+                Rotation2d.fromDegrees(-54)
+            );
+
+            // left center
+            kSOURCE_CENTER_FACES[4] = new Pose2d(
+                0.845,
+                7.44,
+                Rotation2d.fromDegrees(-54)
+            );
+
+            // left bottom
+            kSOURCE_CENTER_FACES[5] = new Pose2d(
+                0.423,
+                7.126,
+                Rotation2d.fromDegrees(-54)
+            );
+        }
+
+        public static final Axis kBARGE_AXIS = 
+            new Axis(
+                8.232,
+                4.343,
+                8.232,
+                7.973
+            );
+        public static final Rotation2d kBARGE_ROTATION = Rotation2d.fromDegrees(0);
+
+        public static final Axis kRIGHT_SOURCE_AXIS = 
+            new Axis(
+                kSOURCE_CENTER_FACES[0].getX(),
+                kSOURCE_CENTER_FACES[0].getY(),
+                kSOURCE_CENTER_FACES[2].getX(),
+                kSOURCE_CENTER_FACES[2].getY()
+            );
+        public static final Rotation2d kRIGHT_SOURCE_AXIS_ROTATION = Rotation2d.fromDegrees(54);
+
+        public static final Axis kLEFT_SOURCE_AXIS = 
+            new Axis(
+                kSOURCE_CENTER_FACES[3].getX(),
+                kSOURCE_CENTER_FACES[3].getY(),
+                kSOURCE_CENTER_FACES[5].getX(),
+                kSOURCE_CENTER_FACES[5].getY()
+            );
+        public static final Rotation2d kLEFT_SOURCE_AXIS_ROTATION = Rotation2d.fromDegrees(-54);
+
+        private AlignmentConstants() {}
+    }
+
+    public static boolean shouldFlipPath() {
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+            return alliance.get() == DriverStation.Alliance.Red;
+        }
+        return false;
+    }
 }
