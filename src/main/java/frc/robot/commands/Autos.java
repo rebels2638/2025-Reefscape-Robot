8bb40f783e55cd15c5b37d0fedde7e166af39b61
Edan Liahovetsky <edan.liahovetsky@icloud.com>
2025-02-19 19:48:07 +0100

    4 l4 auto sim paths and commands

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -5,12 +5,27 @@ import edu.wpi.first.wpilibj2.command.Command;
 public final class Autos {
     private Autos() {}
 
-    public static final Command test = 
+    public static final Command test2 = 
         AutoGenerator.generateAuto(
             "fromProcessorStartToTopRightRB", 
             "RB L4",
             "fromTopRightRBToRightSourceTop",
             "fromRightSourceTopToBottomRightRB",
+            "RB L4"
+        );
+    
+        public static final Command test4 = 
+        AutoGenerator.generateAuto(
+            "fromProcessorStartToTopRightLB", 
+            "LB L4",
+            "fromTopRightLBToRightSourceTop",
+            "fromRightSourceTopToBottomRightLB",
+            "LB L4",
+            "fromBottomRightLBToRightSourceTop",
+            "fromRightSourceTopToBottomRightRB",
+            "RB L4",
+            "fromBottomRightRBToRightSourceTop",
+            "fromRightSourceTopToTopRightRB",
             "RB L4"
         );
 }
