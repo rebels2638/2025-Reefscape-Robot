3cea00b01f0e5f44352e40a8ce78e018d3cf50e3
EtherexStudios <edan.liahovetsky@icloud.com>
2025-03-14 13:03:58 -0400

    stable

--- src/main/java/frc/robot/commands/autoAlignment/source/AlignToClosestSourceLinearAndIntake.java
+++ src/main/java/frc/robot/commands/autoAlignment/source/AlignToClosestSourceLinearAndIntake.java
@@ -1,30 +1,23 @@
-// package frc.robot.commands.autoAlignment.source;
+package frc.robot.commands.autoAlignment.source;
 
-// import edu.wpi.first.math.kinematics.ChassisSpeeds;
-// import edu.wpi.first.wpilibj.XboxController;
-// import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-// import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-// import frc.robot.commands.autoAlignment.LinearAlign;
-// import frc.robot.commands.autoAlignment.LockDriveAxis;
-// import frc.robot.commands.roller.IntakeCoral;
-// import frc.robot.constants.Constants;
-// import frc.robot.lib.util.AlignmentUtil;
-// import frc.robot.subsystems.roller.Roller;
-// public class AlignToClosestSourceLinearAndIntake extends ParallelCommandGroup {
-//     public AlignToClosestSourceLinearAndIntake(XboxController controller) {
-//         addCommands(
-//             new WaitUntilCommand( 
-//                 () -> !Roller.getInstance().inRoller()
-//             ),
-//             new ParallelCommandGroup(
-//                 new IntakeCoral(),
-//                 // new LinearAlign(
-//                 //     () -> AlignmentUtil.getClosestSourcePose(),
-//                 //     () -> new ChassisSpeeds(),
-//                 //     2
-//                 // )
-//                 new LockDriveAxis(controller, Constants.AlignmentConstants.kRIGHT_SOURCE_AXIS)
-//             )
-//         );
-//     }
-// }
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.commands.autoAlignment.LockDriveAxis;
+import frc.robot.commands.roller.IntakeCoral;
+import frc.robot.constants.Constants;
+import frc.robot.lib.input.XboxController;
+import frc.robot.lib.util.AlignmentUtil;
+import frc.robot.subsystems.roller.Roller;
+public class AlignToClosestSourceLinearAndIntake extends ParallelCommandGroup {
+    public AlignToClosestSourceLinearAndIntake(XboxController controller) {
+        addCommands(
+            new WaitUntilCommand( 
+                () -> !Roller.getInstance().inRoller()
+            ),
+            new ParallelCommandGroup(
+                new IntakeCoral(),
+                new LockDriveAxis(controller, Constants.AlignmentConstants.kBARGE_AXIS, AlignmentUtil.getClosestBargePose().getRotation())
+            )
+        );
+    }
+}
